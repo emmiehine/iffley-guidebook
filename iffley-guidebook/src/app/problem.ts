859a0f2147ad0e918f788ff947410d8670b29347
Hine <code@getbraintree.com>
2019-07-19 16:35:51 -0500

    Added problem object, messing with table layout

---
+++ iffley-guidebook/src/app/problem.ts
@@ -0,0 +1,7 @@
+export class Problem {
+  id: number;
+  name: string;
+  grades: string[]; //TODO: make Grades class?
+  holds: number[]; //TODO: make Holds class
+  rules: string;
+}
--- iffley-guidebook/src/app/problems/problems.component.ts
+++ iffley-guidebook/src/app/problems/problems.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Problem } from '../problem';
 
 @Component({
   selector: 'app-problems',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProblemsComponent implements OnInit {
 
+  route: Problem = {
+    id: 1,
+    name: "The Ladder",
+    grades: ["4c","B0"],
+    holds: [1,2,3,4],
+    rules: ""
+  }
   constructor() { }
 
   ngOnInit() {
