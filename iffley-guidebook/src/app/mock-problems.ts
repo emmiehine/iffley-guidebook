1448c454a0532ae0d2f52cacddd84cef98629d93
Hine <code@getbraintree.com>
2019-08-19 08:15:10 -0500

    attempting to display list of mock problems. HTML not complying.

---
+++ iffley-guidebook/src/app/mock-problems.ts
@@ -0,0 +1,15 @@
+import { Problem } from './problem';
+
+export const PROBLEMS: Problem[] = [
+  { id: 1,
+    name: "The Ladder",
+    grades: ["4c","B0"],
+    holds: [1,2,3,4],
+    rules: "" },
+
+  { id: 2,
+    name: "Neeps and Tatties",
+    grades: ["5c","B2"],
+    holds: [45,47,3,59],
+    rules: "No chips" }
+];
--- iffley-guidebook/src/app/problems/problems.component.ts
+++ iffley-guidebook/src/app/problems/problems.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Problem } from '../problem';
+import { PROBLEMS } from '../mock-problems';
 
 @Component({
   selector: 'app-problems',
   templateUrl: './problems.component.html',
   styleUrls: ['./problems.component.css']
 })
-export class ProblemsComponent implements OnInit {
 
-  route: Problem = {
+export class ProblemsComponent implements OnInit {
+  //problem = "problem";
+  
+  problems = PROBLEMS;
+  /*route: Problem = {
     id: 1,
     name: "The Ladder",
     grades: ["4c","B0"],
     holds: [1,2,3,4],
     rules: ""
-  }
+  }*/
   constructor() { }
 
   ngOnInit() {
